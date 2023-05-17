3f2506833de18031c554219e5339c6793e98ce1d
Umar Farooq-Ansari <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>
2023-05-16 22:45:07 -0600

    good

---
+++ script.js
@@ -0,0 +1,30 @@
+// function umar() {
+//     console.log('good morning');
+    
+// }
+
+// ARROW FUNCTIONS
+
+umar = () => {
+    console.log('Good morning');
+}
+
+setTimeout(() => {
+    console.log('We are inside timeout');
+}, 3000);
+umar();
+
+
+// let sum = (a,b) => {
+//     return a+b
+// }
+
+// in this function we not need to write return
+
+let sum = (a,b) =>  a+b;
+let half = a => a/2
+
+
+
+
+
